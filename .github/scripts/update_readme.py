a0b61ee15655e884ad1b87dc8307e8ff77f4162d
Yuqiang Wang <yifang.wangyq@foxmail.com>
2023-08-19 12:02:33 +0800

    Update update_readme.py

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -1,29 +1,38 @@
-import yaml
-from datetime import datetime
+import os
+import re
 
-# 遍历博客目录,解析每个post的日期
-posts = [] 
+# 读取博客目录下的所有二级目录
+blog_directory = 'content/post'  # 替换为你的博客目录路径
+subdirectories = [dir for dir in os.listdir(blog_directory) if os.path.isdir(os.path.join(blog_directory, dir))]
 
-for post in get_posts("content/post"):
-  date = parse_date(post)
-  posts.append({'date': date, 'title': post['title']})
+# 从每个博客目录的index.md文件中提取日期和标题
+blog_posts = []
+for dir in subdirectories:
+    index_path = os.path.join(blog_directory, dir, 'index.md')
+    with open(index_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+        date_match = re.search(r'data:\s*(\d{4}-\d{2}-\d{2})', content)
+        title_match = re.search(r'# (.+)', content)
+        date = date_match.group(1) if date_match else 'N/A'
+        title = title_match.group(1) if title_match else 'Untitled'
+        link = f'https://github.com/yourusername/yourrepo/blob/main/blogs/{dir}/index.md'
+        blog_posts.append({'date': date, 'title': title, 'link': link})
 
-# 按日期排序  
-posts.sort(key=lambda x: x['date'], reverse=True)
-
-# 取最近10篇
-posts = posts[:10] 
+# 排序博客文章并选择最近的10篇
+sorted_posts = sorted(blog_posts, key=lambda x: x['date'], reverse=True)[:10]
 
 # 生成Markdown表格
-table = """
-| Date | Title |
-|-|-| 
+markdown_table = f"""
+| UpdateTime | Title | Summary |
+| ---------- | ----- | ------- |
+{''.join([f"| {post['date']} | [{post['title']}]({post['link']}) |  |\n" for post in sorted_posts])}
 """
 
-for post in posts:
-  table += f"| {post['date']} | {post['title']} |\n"
-  
-# 插入README.md  
-readme = read_file('README.md')  
-readme = insert_after(readme, "<!-- BLOG_DIARY -->", table)
-write_file('README.md', readme)
+# 更新README.md文件
+readme_path = 'README.md'  # 替换为你的README.md路径
+with open(readme_path, 'r', encoding='utf-8') as file:
+    readme_content = file.read()
+    updated_readme = re.sub(r'(## `✍️ Recent Posts`)([\s\S]*?)(\n##)', rf'\1{markdown_table}\3', readme_content)
+
+with open(readme_path, 'w', encoding='utf-8') as file:
+    file.write(updated_readme)
